d12faf44e3ef4cde4264e9ab1436c646bcc62cb8
Fadimana Kilci <fadimekilci07@gmail.com>
2020-12-30 03:36:27 +0300

    Update detect_age_video.py

--- detect_age_video.py
+++ detect_age_video.py
@@ -10,30 +10,28 @@ import os
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 
-# frame video karesi, faceNet yüz dedektörü, ageNet yaş sınıflandırıcı, minConf zayıf yüz tespitlerini filtrelemek için güven eşiği
 def detect_and_predict_age(frame, faceNet, ageNet, genderNet, minConf=0.5):
-	# dedektörün tahmin edeceği yaş gruplarının listesi tanımlanır
+	# dedektörün tahmin edeceği yaş sınıflarının listesi tanımlanır
 	AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
 	# 0=male 1=female
-	GENDER_BUCKETS = ["Male", "Female"]
+	GENDER_BUCKETS = ["Kadin", "Erkek"]
 	results = []
 	# shape(yükseklik × genişlik × diğer boyutlar) [:2] ise ilk iki öğeyi alır(h, w)
 	(h, w) = frame.shape[:2]
-	# girdi, ölçek faktörü(varsayılan 1.0 yani ölçeklendirme yok, sinir ağına uygun boyut, RGB kanal değiştirme işlemi
 	# blob, ortalama çıkarma, normalleştirme ve kanal değiştirme işleminden sonra oluşan girdi resmi
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
-	# Yüz tespiti elde etmek için ağa blob u girdi olarak ver
+	# Yüz tespiti elde etmek için ağa blob girdi olarak verilir
 	faceNet.setInput(blob)
-	# forward() ile katman çıktısını hesaplamak için ileri geçiş yapılır
+	# forward() ile katman çıktısını hesaplamak için katmanlarda ilerlenir
 	detections = faceNet.forward()
 
-	# Yüzlerin etrafındaki tespitler ve çizim kutuları arasında dolaşıyor
+	# Tahmin edilen yüz çerçevesinin içerisinde dolaşılır
 	for i in range(0, detections.shape[2]):
-		# Tespit için bir güven değeri oluşturulur
+		# Tespit için bir güven(olasılık) değeri oluşturulur
 		confidence = detections[0, 0, i, 2]
 		# Güvenin minumum güvenden fazla olmasını sağlayarak zayıf tespitler filtrelenir
 		if confidence > minConf:
-			# Nesne için sınırlama kutusunun (x, y) koordinatlarını hesaplar
+			# Nesne için sınırlama kutusunun (x, y) koordinatları hesaplanır
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
 			# Yüzün yatırım getirisi(ROI) çıkarılır
@@ -44,19 +42,19 @@ def detect_and_predict_age(frame, faceNet, ageNet, genderNet, minConf=0.5):
 			faceBlob = cv2.dnn.blobFromImage(face, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
 			global j
 			j = i
-			# Yaş hakkında tahminler yapılır ve en büyük olasılığa sahip olan atanır
+			# Yaş hakkında tahminler yapılır ve en büyük olasılığa sahip olan sınıf atanır
 			ageNet.setInput(faceBlob)
 			predsA = ageNet.forward()
 			i = predsA[0].argmax()
 			age = AGE_BUCKETS[i]
 			ageConfidence = predsA[0][i]
-			# Cinsiyet hakkında tahminler yapılır ve en büyük olasılığa sahip olan atanır
+			# Cinsiyet hakkında tahminler yapılır ve en büyük olasılığa sahip olan sınıf atanır
 			genderNet.setInput(faceBlob)
 			predsG = genderNet.forward()
 			j = predsG[0].argmax()
 			gender = GENDER_BUCKETS[j]
 			genderConfidence = predsG[0][j]
-			# yüz sınırlayıcı kutu konumundan oluşan bir metin oluşturulur
+			# yüz kutu sınırları ve tahmin edilen sınıflar tutulur
 			d = {"loc": (startX, startY, endX, endY), "age": (age, ageConfidence), "gender": (gender, genderConfidence)}
 			# Sonuç listesi güncellenir
 			results.append(d)
@@ -71,20 +69,21 @@ ap.add_argument("-g", "--gender", required=True, help="cinsiyet dedektörü mode
 ap.add_argument("-c", "--conf", type=float, default=0.5, help="confider zayıf tespitleri filtreleme için minimum olasılık")
 args = vars(ap.parse_args())
 
-print("[INFO] loading face detector model...")
+# Modeller yüklenir
+print("[BILGI] Yüz dedektör modeli yükleniyor...")
 prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
 weightsPath = os.path.sep.join([args["face"], "res10_300x300_ssd_iter_140000.caffemodel"])
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-print("[INFO] loading age detector model...")
+print("[BILGI] Yaş dedektör modeli yükleniyor...")
 prototxtPath = os.path.sep.join([args["age"], "age_deploy.prototxt"])
 weightsPath = os.path.sep.join([args["age"], "age_net.caffemodel"])
 ageNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-print("[INFO] loading gender detector model...")
+print("[BILGI] Cinsiyet dedektör modeli yükleniyor...")
 prototxtPath = os.path.sep.join([args["gender"], "gender.prototxt"])
 weightsPath = os.path.sep.join([args["gender"], "gender_net.caffemodel"])
 genderNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 # Video akışı başlatılır
-print("[INFO] starting video stream...")
+print("[BILGI] Video akışı başlatılıyor...")
 vs = VideoStream(src=0).start()
 # Kamera sensörünün ısınması için zaman verilir
 time.sleep(3.0)
@@ -105,20 +104,12 @@ while True:
 		cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (205, 205, 0), 2)
 	# frame çıktısını ekranda göster
 	cv2.imshow("Frame", frame)
-	# cv2.waitKey kodu klavyeyle bağlayan işlevdir.
-	# Son 8 bit klavye girişinde ASCII değerini temsil eder. waitKey çıktısı & 0xFF ile son 8 bite erişilebilir.
 	# Klavyeden basılan tuşun ASCII değeri key değişkenine atanmış olur
 	key = cv2.waitKey(1) & 0xFF
-	# close anlamına gelen "c" tuşuna basılırsa döngüden çık yani frame i kapat
-	# ord("char") maksimum 255 olan karakterin ASCII değerini döndürür
+	# close anlamına gelen "c" tuşuna basılırsa frame kapatılır
 	if key == ord("c"):
 		break
 
 # windowstaki cv2 değişkenine ait her şeyi temizler
 cv2.destroyAllWindows()
 vs.stop()
-
-
-
-# python detect_age_video.py --face face_detector --age age_detector --gender gender_detector
-# Terminele yukarıdaki ifade girilerek argparse ile oluşturulan değişkenlere eğtim dosyalarımız atanır
